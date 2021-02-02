02c60d4d132e3f4837e181053ceed6264506516d
Luca Realdi <lr@opencontent.it>
2021-02-02 14:00:27 +0100

    Disabilita il connettore recaptcha se l'utente corrente è autenticato

--- classes/FieldConnectors/OcRecaptchaField.php
+++ classes/FieldConnectors/OcRecaptchaField.php
@@ -4,22 +4,50 @@ use Opencontent\Ocopendata\Forms\Connectors\OpendataConnector\FieldConnector;
 
 class OcRecaptchaField extends FieldConnector
 {
+    private function isEnabled()
+    {
+        return !eZUser::isCurrentUserRegistered();
+    }
+
     public function getData()
     {
-        return null;
+        return !$this->isEnabled() ? 'ok' : null;
     }
+
+    public function getSchema()
+    {
+        $schema = parent::getSchema();
+        if (!$this->isEnabled()) {
+            $schema['required'] = false;
+        }
+
+        return $schema;
+    }
+
 
     public function getOptions()
     {
-        return array(
-            "helper" => $this->attribute->attribute('description'),
-            "type" => 'recaptcha',
-            "sitekey" => $this->attribute->attribute(OcReCaptchaType::PUBLIC_KEY_FIELD)
-        );
+        if (!$this->isEnabled()){
+            $options = array(
+                "type" => 'hidden',
+            );
+        }else {
+            $options = array(
+                "helper" => $this->attribute->attribute('description'),
+                "type" => 'recaptcha',
+                "sitekey" => $this->attribute->attribute(OcReCaptchaType::PUBLIC_KEY_FIELD)
+            );
+        }
+
+        return $options;
     }
 
     public function setPayload($postData)
     {
+        if (!$this->isEnabled()) {
+            return 'ok';
+        }
+
         if (!empty($postData)){
             $isValid = OcReCaptchaType::validateCaptcha(
                 $postData,
